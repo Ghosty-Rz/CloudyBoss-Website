cf5d858d3dc9ce311c266b6359b01003e87c3a19
Rihab Zouitni <107633200+Ghosty-Rz@users.noreply.github.com>
2024-07-11 17:50:21 +0900

    Keystone page + Hovering over dropdown menu
    Created the Keystone page + Enabled the hovering display over the dropdown menus

--- app.js
+++ app.js
@@ -11,21 +11,55 @@ const mobileMenu = () => {
 menuIcon.addEventListener('click', mobileMenu);
 closeIcon.addEventListener('click', mobileMenu);
 
+// document.addEventListener('DOMContentLoaded', function () {
+//     const links = document.querySelectorAll('.dropdown-link');
+//     const columns = document.querySelectorAll('.menu-dropdown-column');
+
+//     links.forEach(link => {
+//         link.addEventListener('mouseover', function (event) {
+//             event.preventDefault();
+//             const target = this.getAttribute('data-detail');
+//             // console.log('Target:', target); // Check if target is correctly retrieved
+//             columns.forEach(column => {
+//                 column.classList.remove('active');
+//                 console.log('id:', column.id);
+//                 if (column.id == `${target}-details`) {
+//                     column.classList.add('active');
+//                 }
+//             });
+//         });
+//     });
+// });
+
+
+// THIS IS USED TO DISPLAY THE DETAILS//
+//ON THE RIGHT SIDE 0F THE DROPDOWN MENU//
+
 document.addEventListener('DOMContentLoaded', function () {
     const links = document.querySelectorAll('.dropdown-link');
-    const detailSections = document.querySelectorAll('.detail-section');
+    const details = document.querySelectorAll('.nav-details');
 
     links.forEach(link => {
-        link.addEventListener('click', function (event) {
+        link.addEventListener('mouseover', function (event) {
             event.preventDefault();
             const target = this.getAttribute('data-detail');
-            detailSections.forEach(section => {
-                if (section.id === target) {
-                    section.classList.add('active');
-                } else {
-                    section.classList.remove('active');
-                }
+            console.log('Target:', target);
+
+            // Hide all details initially
+            details.forEach(detail => {
+                detail.classList.remove('active');
             });
+        
+
+            // Show the detail corresponding to the target
+            const targetDetail = document.getElementById(`${target}-details`);
+            if (targetDetail) {
+                targetDetail.classList.add('active');
+            }
         });
     });
 });
+
+
+
+
